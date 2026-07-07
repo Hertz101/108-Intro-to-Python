1fc9a0581209167ccb1de545dc00005090ced2ff
Adam Hertzberg <Adamh4880@sbcglobal.net>
2026-07-06 19:27:33 -0600

    added dog donverter

--- dog_age_converter.py
+++ dog_age_converter.py
@@ -1,6 +1,6 @@
 # Used a basic print() to create the title below in the terminal.
 print("-----------Dog Age Converter------------")
-
+print("")
 # Created an input function with int(Integer) then prompt the user to input the age of their dog.
 dog_age = int(input("Enter your dog's age in human years:"))
 
@@ -10,3 +10,4 @@ dog_years = dog_age * 7
 """Created an f String telling the user how old thier dog is by adding the results (age) from
 #  the input (dog_age x 7 = age) followed by the rest of the print "years old in dog years" """
 print(f"Your dog is {dog_years} years old in dog years!")
+print("")
